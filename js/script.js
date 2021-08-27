8277cdbc8fe128cc77747a7ed1365fce872d289e
Alba <albadvg@yahoo.es>
2021-08-27 12:21:13 +0200

    Commit inicial

---
+++ js/script.js
@@ -0,0 +1,62 @@
+
+$("#carousel-rehab").owlCarousel({
+    items:4,
+    nav:true,
+    merge:true,
+    dots: false,
+    lazyLoad:true,
+    responsive: {
+        0: {
+            items: 1,
+
+        },
+
+        481: {
+            items : 2,
+        },
+
+        789: {
+            items: 3,
+        },
+
+        960: {
+            items: 4,
+        },
+    }
+});
+
+$(".carousel-estancias").owlCarousel({
+    items:1,
+    nav:true,
+    
+});
+
+$('#carousel-cursos').owlCarousel({
+    items:3,
+    nav:true,
+    dots:false,
+    responsive: {
+        0: {
+            items: 1,
+
+        },
+
+        481: {
+            items : 2,
+        },
+
+        789: {
+            items: 3,
+        },
+
+        960: {
+            items: 3,
+        },
+    }
+});
+
+$(".carousel-entorno").owlCarousel({
+    items:1,
+    nav:true,
+    
+});
